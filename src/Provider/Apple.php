c5654e641c7909097f6f189517a276cbd362845d
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-03-23 00:08:24 +0400

    Update Apple.php

--- src/Provider/Apple.php
+++ src/Provider/Apple.php
@@ -199,8 +199,12 @@ class Apple extends AbstractProvider
     protected function checkResponse(ResponseInterface $response, $data)
     {
         if ($response->getStatusCode() >= 400) {
+            $message = array_key_exists('error', $data) ? $data['error'] : $response->getReasonPhrase();
+            if (array_key_exists('error_description', $data)) {
+                $message .= ': ' . $data['error_description'];
+            }
             throw new AppleAccessDeniedException(
-                array_key_exists('error', $data) ? $data['error'] : $response->getReasonPhrase(),
+                $message,
                 array_key_exists('code', $data) ? $data['code'] : $response->getStatusCode(),
                 $response
             );
